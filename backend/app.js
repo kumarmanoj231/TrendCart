ccb2c915934234647883a1805d495d8ee4e7b5d0
kumarmanoj231 <lodhamanoj383@gmail.com>
2025-02-23 10:10:26 +0530

    initail setup

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+// packages
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+
+// utils
+import connectDB from './config/db.js';
+
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.listen(PORT, () => {
+    console.log(`app is listening port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('API is running....');
+});
